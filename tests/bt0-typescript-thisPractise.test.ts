85437c519839b6cee92dc388a39cf42ee4025a27
trangnct-3275 <nguyen.cao.thuy.trang@sun-asterisk.com>
2026-02-09 13:04:24 +0700

    BTTH Typescript Language - thisPractise

---
+++ tests/bt0-typescript-thisPractise.test.ts
@@ -0,0 +1,22 @@
+import { test } from '@playwright/test';
+class Counter {
+  count: number = 0;
+
+  increment(): void {
+      this.count++;
+      console.log(`Count is now: ${this.count}`);
+  }
+
+  reset(): void {
+    this.count = 0;
+    console.log('Reset count');
+  }
+}
+
+test('Demo', async () => {
+  const counter = new Counter();
+  counter.increment();
+  counter.increment();
+  counter.reset();
+});
+
