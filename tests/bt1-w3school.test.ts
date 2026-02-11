85ecfbe75935e0dd0d2292b2f5e30abc9f5e3c79
trangnct-3275 <nguyen.cao.thuy.trang@sun-asterisk.com>
2026-02-11 11:56:28 +0700

    Typescript Execies

--- tests/bt1-w3school.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Open W3school page', async ({ page }) => {
-  await page.goto('https://www.w3schools.com/');
-  await page.fill('#tnb-google-search-input', 'testuser');
-  await page.locator('#tnb-google-search-icon').click();
-});
